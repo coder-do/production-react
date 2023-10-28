9a7dd0657e9ff64a4f84737cb6a35e2204f83114
Meruzh <meruzh.kiloyan@mail.ru>
2023-10-28 13:41:11 +0400

    1.10 fsd preparing

--- config/build/buildResolvers.ts
+++ config/build/buildResolvers.ts
@@ -1,7 +1,11 @@
-import {ResolveOptions} from "webpack";
+import path from "path";
+import { ResolveOptions } from "webpack";
 
 export function buildResolvers(): ResolveOptions {
     return {
         extensions: ['.tsx', '.ts', '.js'],
+        preferAbsolute: true,
+        modules: [options.paths.src, 'node_modules'],
+        mainFiles: ['index'],
+        alias: {}
     }
-}
