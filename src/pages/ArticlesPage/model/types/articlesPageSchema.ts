5a125ef20fabb926d44b21999905b297e618b6d4
Meruzh <meruzh.kiloyan@mail.ru>
2023-11-20 13:43:10 +0400

    module 8 done

--- src/pages/ArticlesPage/model/types/articlesPageSchema.ts
+++ src/pages/ArticlesPage/model/types/articlesPageSchema.ts
@@ -2,8 +2,11 @@ import { EntityState } from '@reduxjs/toolkit';
 import { Article, ArticleView } from 'entities/Article';
 
 export interface ArticlesPageSchema extends EntityState<Article> {
-    isLoading?: boolean;
-    error?: string;
+  isLoading?: boolean;
+  error?: string;
 
-    view: ArticleView;
+  view: ArticleView;
+  page: number;
+  limit?: number;
+  hasMore: boolean;
 }
