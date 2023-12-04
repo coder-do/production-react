0cd6241439ed7208f3331ffe21f8e3d66d49c371
Meruzh <meruzh.kiloyan@mail.ru>
2023-12-04 16:57:22 +0400

    10.63 done

--- src/shared/ui/Page/Page.tsx
+++ src/shared/ui/Page/Page.tsx
@@ -2,15 +2,14 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import {
   memo, MutableRefObject, ReactNode, UIEvent, useRef,
 } from 'react';
-import { useLocation } from 'react-router-dom';
 import { useInfiniteScroll } from 'shared/lib/hooks/useInfiniteScroll/useInfiniteScroll';
-import { getUIScrollByPath, uiActions } from 'features/UI';
 import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
+import { getUIScrollByPath, uiActions } from 'features/UI';
+import { useLocation } from 'react-router-dom';
 import { useInitialEffect } from 'shared/lib/hooks/useInitialEffect/useInitialEffect';
 import { useSelector } from 'react-redux';
 import { StateSchema } from 'app/providers/StoreProvider';
 import { useThrottle } from 'shared/lib/hooks/useThrottle/useThrottle';
-
 import cls from './Page.module.scss';
 
 interface PageProps {
@@ -19,13 +18,17 @@ interface PageProps {
   onScrollEnd?: () => void;
 }
 
+export const PAGE_ID = 'PAGE_ID';
+
 export const Page = memo((props: PageProps) => {
-  const { pathname } = useLocation();
-  const dispath = useAppDispatch();
   const { className, children, onScrollEnd } = props;
   const wrapperRef = useRef() as MutableRefObject<HTMLDivElement>;
   const triggerRef = useRef() as MutableRefObject<HTMLDivElement>;
-  const scrollPosition = useSelector((state: StateSchema) => getUIScrollByPath(state, pathname));
+  const dispatch = useAppDispatch();
+  const { pathname } = useLocation();
+  const scrollPosition = useSelector(
+    (state: StateSchema) => getUIScrollByPath(state, pathname),
+  );
 
   useInfiniteScroll({
     triggerRef,
@@ -38,9 +41,9 @@ export const Page = memo((props: PageProps) => {
   });
 
   const onScroll = useThrottle((e: UIEvent<HTMLDivElement>) => {
-    dispath(uiActions.setScrollPosition({
+    dispatch(uiActions.setScrollPosition({
+      position: e.currentTarget.scrollTop,
       path: pathname,
-      position: +e.currentTarget.scrollTop,
     }));
   }, 500);
 
@@ -49,6 +52,7 @@ export const Page = memo((props: PageProps) => {
       ref={wrapperRef}
       className={classNames(cls.Page, {}, [className])}
       onScroll={onScroll}
+      id={PAGE_ID}
     >
       {children}
       {onScrollEnd ? <div className={cls.trigger} ref={triggerRef} /> : null}
